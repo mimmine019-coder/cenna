985a742eea117bfe8fbc01bf719249398ff0be81
mimmine019-coder <mimmine019@gmail.com>
2026-09-20 00:23:20 +0000

    Complete authentication routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,18 @@ import {
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Account from './pages/Account'
+
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* =========================
+            PUBLIC
+        ========================= */}
 
         <Route
           path="/"
@@ -26,6 +33,19 @@ function App() {
         <Route
           path="/register"
           element={<Register />}
+        />
+
+        {/* =========================
+            PROTECTED
+        ========================= */}
+
+        <Route
+          path="/account"
+          element={
+            <ProtectedRoute>
+              <Account />
+            </ProtectedRoute>
+          }
         />
 
       </Routes>
